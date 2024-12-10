7370aa0c344adad1d525cc1d8ff5960d7062c3f3
Bastian Waidelich <b.waidelich@wwwision.de>
2024-12-10 20:19:25 +0100

    Major overhaul and simplification

--- src/DoctrineEventStream.php
+++ src/DoctrineEventStream.php
@@ -10,14 +10,8 @@ use Traversable;
 use Webmozart\Assert\Assert;
 use Wwwision\DCBEventStore\EventStream;
 use Wwwision\DCBEventStore\Types\Event;
-use Wwwision\DCBEventStore\Types\EventData;
 use Wwwision\DCBEventStore\Types\EventEnvelope;
-use Wwwision\DCBEventStore\Types\EventId;
-use Wwwision\DCBEventStore\Types\EventMetadata;
-use Wwwision\DCBEventStore\Types\EventType;
 use Wwwision\DCBEventStore\Types\SequenceNumber;
-use Wwwision\DCBEventStore\Types\StreamQuery\CriterionHashes;
-use Wwwision\DCBEventStore\Types\Tags;
 
 final class DoctrineEventStream implements EventStream
 {
@@ -55,13 +49,11 @@ final class DoctrineEventStream implements EventStream
         return new EventEnvelope(
             SequenceNumber::fromInteger((int)$row['sequence_number']),
             $recordedAt,
-            $row['criterion_hashes'] === '' ? CriterionHashes::none() : CriterionHashes::fromArray(explode(',', $row['criterion_hashes'])),
-            new Event(
-                EventId::fromString($row['id']),
-                EventType::fromString($row['type']),
-                EventData::fromString($row['data']),
-                Tags::fromJson($row['tags']),
-                $row['metadata'] === null ? EventMetadata::none() : EventMetadata::fromJson($row['metadata']),
+            Event::create(
+                $row['type'],
+                $row['data'],
+                $row['tags'],
+                $row['metadata'],
             ),
         );
     }
