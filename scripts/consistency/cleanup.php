0e27b1b268a43ec5d639a71fea992b61dae48ee9
Bastian Waidelich <b.waidelich@wwwision.de>
2026-04-18 10:28:46 +0200

    BUGFIX: Validate events during cleanup

--- scripts/consistency/cleanup.php
+++ scripts/consistency/cleanup.php
@@ -1,4 +1,13 @@
 <?php
 
 require __DIR__ . '/../../vendor/autoload.php';
+$caughtException = null;
+try {
+    \Wwwision\DCBEventStoreDoctrine\Tests\Integration\ConcurrencyTest::validateEvents();
+} catch (Throwable $e) {
+    $caughtException = $e;
+}
 \Wwwision\DCBEventStoreDoctrine\Tests\Integration\ConcurrencyTest::cleanup();
+if ($caughtException !== null) {
+    throw $caughtException;
+}
